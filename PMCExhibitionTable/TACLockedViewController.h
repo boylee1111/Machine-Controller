f32beee642b356cf727c4563499ecec87ca742cf
Nathan <shaozeman@qq.com>
2014-01-10 16:04:32 +0800

    解锁界面完成

---
+++ PMCExhibitionTable/TACLockedViewController.h
@@ -0,0 +1,15 @@
+//
+//  TACLockedViewController.h
+//  PMCExhibitionTable
+//
+//  Created by Nathan on 14-1-10.
+//  Copyright (c) 2014年 com.nathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MBSliderView.h"
+
+@interface TACLockedViewController : UIViewController<MBSliderViewDelegate>
+
+
+@end
