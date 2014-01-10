3a0629007c9c9a537cdfb1c5ddccc24bb3bbff0d
Boyi Lee <boylee1111@gmail.com>
2014-01-10 20:06:16 +0800

    AsyncSocket bug fixed. 添加LockedViewController TCP/IP连接

--- PMCExhibitionTable/TACLockedViewController.m
+++ PMCExhibitionTable/TACLockedViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "TACLockedViewController.h"
+#import "CommandExporter.h"
 
-@interface TACLockedViewController ()
+@interface TACLockedViewController () {
+    GCDAsyncSocket *asyncSocket;
+}
 
 @end
 
@@ -27,6 +30,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    asyncSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    
     MBSliderView *s1 = [[MBSliderView alloc] initWithFrame:CGRectMake(285, 440, 450, 44.0)];
     [s1 setText:@">>>"]; // set the label text
 //    [s1 setThumbColor:[UIColor colorWithRed:166.0/255.0 green:166.0/255.0 blue:166.0/255.0 alpha:1.0]];
@@ -34,7 +40,24 @@
     [s1 setDelegate:self]; // set the MBSliderView delegate
     [self.view addSubview:s1];
 }
-// MBSliderViewDelegate
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    NSError *err = nil;
+    if (![asyncSocket connectToHost:LOCAL_IP_ADDRESS
+                             onPort:PORT
+                        withTimeout:5
+                              error:&err]) {
+        NSLog(@"connect error %@", err);
+    }
+}
+
+#pragma mark - Delegates
+
+#pragma mark MBSliderViewDelegate Delegate
+
 - (void) sliderDidSlide:(MBSliderView *)slideView {
     // Customization example
     [self dismissViewControllerAnimated:UIModalTransitionStyleCrossDissolve completion:nil];
@@ -44,6 +67,18 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark GCDAsyncSocket Delegate
+
+- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host
+          port:(uint16_t)port {
+    NSLog(@"Connecting successfully. Demo mode begins.");
+}
+
+- (void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag
+{
+    NSLog(@"Wirting data tag = %ld", tag);
 }
 
 @end
